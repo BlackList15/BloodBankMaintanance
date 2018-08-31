12bb272614ae2f5ebd8f5a19496554d25e8a1b0d
[Aashik96] <[aashik.ahamed12@gmail.com]>
2018-08-31 11:28:16 +0530

    jdatechooser plugin added

--- Database/bloodbank.sql
+++ Database/bloodbank.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.0.4
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 30, 2018 at 07:43 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Host: localhost
+-- Generation Time: Aug 31, 2018 at 05:53 AM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `bloodbank`
 --
+CREATE DATABASE IF NOT EXISTS `bloodbank` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bloodbank`;
 
 -- --------------------------------------------------------
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `hospital`
 --
 
-CREATE TABLE `hospital` (
+CREATE TABLE IF NOT EXISTS `hospital` (
   `id` int(11) NOT NULL,
   `hosName` varchar(40) NOT NULL,
   `hosContact` int(10) NOT NULL,
@@ -51,9 +51,10 @@ INSERT INTO `hospital` (`id`, `hosName`, `hosContact`, `address`, `disFromHere`,
 -- Table structure for table `login`
 --
 
-CREATE TABLE `login` (
+CREATE TABLE IF NOT EXISTS `login` (
   `id` char(6) NOT NULL,
-  `password` varchar(15) NOT NULL
+  `password` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -61,35 +62,7 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`id`, `password`) VALUES
-('ADM001', 'admin001@bb'),
-('ADM002', '#Karshan96');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `hospital`
---
-ALTER TABLE `hospital`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `hospital`
---
-ALTER TABLE `hospital`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
+('ADM001', 'admin001@bb');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
